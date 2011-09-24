bbe279331a5a5956d92ca6f5c41cc8dcf99b301c
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2011-09-24 01:58:43 -0500

    docs

--- lib/Catalyst/Controller/Accessors.pm
+++ lib/Catalyst/Controller/Accessors.pm
@@ -3,6 +3,8 @@ package Catalyst::Controller::Accessors;
 use strict;
 use warnings;
 
+# ABSTRACT: Accessors for a namespaced stash
+
 use Carp 'croak';
 
 Moose::Exporter->setup_import_methods(
@@ -34,3 +36,65 @@ sub cat_has {
 }
 
 1;
+
+=pod
+
+=head1 SYNOPSIS
+
+ package MyApp::Controller::Users::Roles;
+
+ use Moose;
+ use Catalyst::Controller::Accessors;
+
+ use namespace::autoclean;
+
+ BEGIN { extends 'Catalyst::Controller' };
+
+ cat_has user => (
+   is => 'ro',
+   namespace => 'MyApp::Controller::Users',
+ );
+
+ cat_has $_ => ( is => 'rw' ) for qw(resultset thing);
+
+ sub load_rs {
+   my ($self, $c) = @_;
+
+   $self->resultset($c,
+      $self->user($c)->roles
+   );
+ }
+
+ sub load_thing {
+   my ($self, $c, $id) = @_;
+
+   $self->thing($c,
+      $self->resultset($c)->find($id)
+   );
+ }
+
+ sub get_user {
+   my ($self, $c) = @_;
+
+   $c->response->body($self->thing($c)->name);
+ }
+
+=head1 DESCRIPTION
+
+The overall idea for this module is to allow more sensible access to the stash.
+It merely namespaces the stash based on the name of the controller adding the
+accessor or the specified namespace.  It's prime purpose is for chaining.
+
+=head1 IMPORTED SUBROUTINES
+
+=head2 cat_has
+
+Options:
+
+=over 1
+
+=item * C<is> - B<required>, must be either C<ro> or C<rw>
+
+=item * C<namespace> - defaults to current controller
+
+=back
